9871024cc538769c5417a39b333f4b6bf23f4233
Maxim <s_maxim_v@bk.ru>
2020-07-22 01:20:44 +0300

    Remove trash

--- Projects/TableView using MVVM/GreenGrocery/Home/Cell/GroceryItemCell.swift
+++ Projects/TableView using MVVM/GreenGrocery/Home/Cell/GroceryItemCell.swift
@@ -22,17 +22,6 @@ class GroceryItemCell: UITableViewCell {
     
     @IBOutlet weak var priceLabel: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
     func configure(usingViewModel viewModel: GroceryItemViewModel) {
         
         self.productImageView.backgroundColor = UIColor.init(
